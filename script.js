ae9745e27633dd3fccc04be36c3ef5f278c478b6
Laurentius Dika <39826578+Lcurents@users.noreply.github.com>
2023-10-17 14:24:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const dynamicText = document.querySelector('.teksdiam span');
-const words = [
-  'Web Developer',
-  'Mobile Developer',
-  'Illustrator',
-  'Graphic Designer',
-];
+const words = ['Web Developer', 'Mobile Developer', 'Illustrator'];
 
 let wordIndex = 0;
 let charIndex = 0;
@@ -31,3 +26,33 @@ const typeEffect = () => {
   }
 };
 typeEffect();
+
+function tambahkanBreak() {
+  const h3Elem = document.querySelector('.teksdiam');
+  const spanElem = h3Elem.querySelector('span');
+
+  if (window.innerWidth <= 991.98) {
+    const brElem = document.createElement('br');
+    h3Elem.insertBefore(brElem, spanElem);
+  }
+}
+
+// Panggil fungsi setelah halaman dimuat
+window.onload = tambahkanBreak;
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hiddenleft');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElementscenter = document.querySelectorAll('.hiddencenter');
+hiddenElementscenter.forEach((el) => observer.observe(el));
